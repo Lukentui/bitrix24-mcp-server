977a9a61c813bcc3bcbbfa3f1c8e9f136f5137c6
Luka Nikitin <lukentuichennel@gmail.com>
2026-05-09 21:58:56 +0300

    chore: create empty branch

--- build/config/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const config: {
-    BITRIX24_WEBHOOK_URL: string;
-    NODE_ENV: "development" | "production";
-    LOG_LEVEL: "error" | "debug" | "info" | "warn";
-};
-//# sourceMappingURL=index.d.ts.map
